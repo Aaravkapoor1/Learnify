e0ec2e81b3151fb3eae8834ed31401bc63f15126
Aarav Kapoor <aaravkapoor1221@gmail.com>
2025-05-22 11:01:15 +0530

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,278 @@
+const navLinks = [
+  {
+    name: "About Us",
+    link: "#about us",
+  },
+  {
+    name: "Courses",
+    link: "#courses",
+  },
+  {
+    name: "Partners",
+    link: "#partners",
+  },
+  {
+    name: "Feedback",
+    link: "#feedback",
+  },
+];
+
+const words = [
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+];
+
+const counterItems = [
+  { value: 9, suffix: "+", label: "Courses Covered" },
+  { value: 2000, suffix: "+", label: "Student Patners" },
+  { value: 500, suffix: "+", label: "Questions Available" },
+  { value: 95, suffix: "%", label: "Positive Feedback" },
+];
+
+const logoIconsList = [
+  {
+    imgPath: "/images/logos/company-logo-1.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-2.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-3.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-4.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-5.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-6.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-7.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-8.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-9.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-10.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-11.png",
+  },
+];
+
+const abilities = [
+  {
+    imgPath: "/images/seo.png",
+    title: "Quality Focus",
+    desc: "Delivering high-quality results while maintaining attention to every detail.",
+  },
+  {
+    imgPath: "/images/chat.png",
+    title: "Reliable Communication",
+    desc: "Keeping you updated at every step to ensure transparency and clarity.",
+  },
+  {
+    imgPath: "/images/time.png",
+    title: "On-Time Support",
+    desc: "Our Teachers and Mentors will reach out to you in no time to help.",
+  },
+];
+
+const techStackImgs = [
+  {
+    name: "React Developer",
+    imgPath: "/images/logos/react.png",
+  },
+  {
+    name: "Python Developer",
+    imgPath: "/images/logos/python.svg",
+  },
+  {
+    name: "Backend Developer",
+    imgPath: "/images/logos/node.png",
+  },
+  {
+    name: "Interactive Developer",
+    imgPath: "/images/logos/three.png",
+  },
+  {
+    name: "Project Manager",
+    imgPath: "/images/logos/git.svg",
+  },
+];
+
+const techStackIcons = [
+  {
+    name: "React Developer",
+    modelPath: "/models/react_logo-transformed.glb",
+    scale: 1,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Python Developer",
+    modelPath: "/models/python-transformed.glb",
+    scale: 0.8,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Backend Developer",
+    modelPath: "/models/node-transformed.glb",
+    scale: 5,
+    rotation: [0, -Math.PI / 2, 0],
+  },
+  {
+    name: "Interactive Developer",
+    modelPath: "/models/three.js-transformed.glb",
+    scale: 0.05,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Project Manager",
+    modelPath: "/models/git-svg-transformed.glb",
+    scale: 0.05,
+    rotation: [0, -Math.PI / 4, 0],
+  },
+];
+
+const expCards = [
+  {
+    review: "We are enlighted to work with Learnify, Giving back to the community by helping them make their career roots more sterdy is one of a good deed in itself indeed",
+    imgPath: "/images/exp1.png",
+    logoPath: "/images/logo1.png",
+    title: "UI/UX Partner",
+    date: "January 2025 - Present",
+    responsibilities: [
+      "Developed and maintained user-facing features for the Learnify website.",
+      "Collaborated closely with UI/UX designers to ensure seamless user experiences.",
+      "Optimized web applications for maximum speed and scalability.",
+    ],
+  },
+  {
+    review: "Learnify has focused on developing the best dashboard for it's students. Delivering the best experience ever !",
+    imgPath: "/images/exp2.png",
+    logoPath: "/images/logo2.png",
+    title: "Dashboard Development Partner",
+    date: "Jannuary 2025 - Present",
+    responsibilities: [
+      "Led the development of Dashboard and it's web applications, focusing on scalability.",
+      "Worked with backend engineers to integrate APIs seamlessly with the frontend.",
+      "Contributed to open-source projects that were used with the Docker ecosystem.",
+    ],
+  },
+  {
+    review: "Learnify focuses on the Quality of notes and assignments",
+    imgPath: "/images/exp3.png",
+    logoPath: "/images/logo3.png",
+    title: "Education content",
+    date: "Jannuary 2025 - present",
+    responsibilities: [
+      "Built cross-platform services, making notes easily accessible.",
+      "Improved app performance and user experience through code optimization and testing.",
+      "Coordinated with the product team to implement features based on feedback.",
+    ],
+  },
+];
+
+const expLogos = [
+  {
+    name: "logo1",
+    imgPath: "/images/logo1.png",
+  },
+  {
+    name: "logo2",
+    imgPath: "/images/logo2.png",
+  },
+  {
+    name: "logo3",
+    imgPath: "/images/logo3.png",
+  },
+];
+
+const testimonials = [
+  {
+    name: "Amit Singh",
+    mentions: "@Amit.S",
+    review:
+      "Learnify has literally saved my grades! The notes are super well-organized and easy to understand, even for complicated topics. Plus, the practice questions help me prep for exams without feeling overwhelmed. Total game-changer for college life!",
+    imgPath: "/images/client1.png",
+  },
+  {
+    name: "Rohan Maheshwari",
+    mentions: "@Rohan.M12",
+    review:
+      "I love how easy it is to find what I need on Learnify. Whether it’s last-minute notes or mock questions before a test, everything’s just a few clicks away. The platform is clean, super student-friendly, and genuinely helpful.",
+    imgPath: "/images/client3.png",
+  },
+  {
+    name: "Arjun Dutta",
+    mentions: "@who.is.Arjun",
+    review:
+     "What sets Learnify apart is how updated and relevant the notes are. It's not just some random PDFs dumped together — everything feels curated for actual college exams. It’s now my go-to resource for studying.",
+    imgPath: "/images/client2.png",
+  },
+  {
+    name: "Marvin McKinney",
+    mentions: "@marvinmckinney",
+    review:
+      "Adrian was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
+    imgPath: "/images/client5.png",
+  },
+  {
+    name: "Arjun Reddy",
+    mentions: "@flo.Arjun",
+    review:
+      "From class notes to tough questions — Learnify covers it all! It's the perfect blend of simplicity and depth. I also love that they focus exactly on what we need to crack our college exams, without unnecessary fluff.",
+    imgPath: "/images/client4.png",
+  },
+  {
+    name: "Varun Ranjan",
+    mentions: "@Varunranjan",
+    review:
+      "From class notes to tough questions — Learnify covers it all! It's the perfect blend of simplicity and depth. I also love that they focus exactly on what we need to crack our college exams, without unnecessary fluff.",
+    imgPath: "/images/client6.png",
+  },
+];
+
+const socialImgs = [
+  {
+    name: "insta",
+    imgPath: "/images/insta.png",
+  },
+  {
+    name: "fb",
+    imgPath: "/images/fb.png",
+  },
+  {
+    name: "x",
+    imgPath: "/images/x.png",
+  },
+  {
+    name: "linkedin",
+    imgPath: "/images/linkedin.png",
+  },
+];
+
+export {
+  words,
+  abilities,
+  logoIconsList,
+  counterItems,
+  expCards,
+  expLogos,
+  testimonials,
+  socialImgs,
+  techStackIcons,
+  techStackImgs,
+  navLinks,
+};
